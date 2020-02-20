ad68d46889ae63f793e3673290e4ea66e0eb180e
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-02-20 12:29:43 -0800

    added styling to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,32 @@
 import React from "react";
 import logo from "../img/nasa_logo.svg";
-import background from "../img/headerbg.jpg";
+import background from "../img/astronaut.jpg";
 import styled from "styled-components";
+import "../App.css";
 
+const StyledLogo = styled.img`
+max-width: 200px;
+margin-top: 3%;
+`;
+
+const StyledHeader = styled.header`
+background: url(${ background });
+`;
+
+const StyledHeading = styled.h1`
+    color: white;
+    padding-bottom: 2%;
+    font-family: 'Teko', sans-serif;
+    letter-spacing: 2px;
+`;
 
 
 export default function Header() {
-
-    const StyledLogo = styled.img`
-        max-width: 200px;
-        margin-top: 3%;
-    `;
-
-    const StyledHeader = styled.header`
-        background: navy;
-    `;
-
     return (
 
-        <StyledHeader styles = {{backgroundImage: `url(${background}`}}>
+        <StyledHeader>
             <StyledLogo src={logo} alt="nasa logo"/>
-            <h1>Astronomy Picture of the Day</h1>
+            <StyledHeading>Astronomy Picture of the Day</StyledHeading>
         </StyledHeader>
     )
 }
